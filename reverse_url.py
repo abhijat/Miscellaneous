aa0b5b7244b207a02c2985b0fa3048b87c4a3430
Abhijat Malviya <malviya.abhijat@gmail.com>
2012-04-06 15:55:43 +0530

    Added square root code

--- reverse_url.py
+++ reverse_url.py
@@ -3,6 +3,7 @@
 import sys,urllib, re, string
 from BeautifulSoup import BeautifulSoup
 from BeautifulSoup import BeautifulStoneSoup as bss
+from subprocess import *
 
 imgurl = sys.argv[1]
 print "Getting page title.."
@@ -19,6 +20,20 @@ print "Searching on reddit.."
 new_soup = BeautifulSoup(urllib.urlopen(newurl))
 
 results = new_soup.findAll('a', {"class" : "comments"})
+if len(results) == 0:
+    print 'Nothing found, sorry.' 
+    sys.exit(1)
 print "Found top result:\n", results[0]
 go_here = results[0]['href']
 print go_here
+
+commands = [
+        'open',
+        '-a',
+        'Google Chrome',
+        go_here
+        ]
+proc = Popen(commands,stdout=PIPE,stderr=PIPE)
+(out,err) = proc.communicate()
+print out, err
+sys.exit(0)
